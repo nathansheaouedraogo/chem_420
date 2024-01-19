21d034ee0c87836df484d4ed05cc952d8a082f6f
unknown <nas779@usask.ca>
2024-01-19 10:55:52 -0600

    no clue why the df is not working. importing 2 excel and converting to csv!

--- analysis.py
+++ analysis.py
@@ -1,7 +1,9 @@
 # get data frames and such; reverse fft!
 import plot
 import fft_df
+import cwd
 
-print(fft_df.fft_df())
-
+df = fft_df.fft_df()
+df.to_csv(cwd.file_path('df'), index=False)
+print(df)
 # plot.visualize_fft(fft_df.fft_df())
